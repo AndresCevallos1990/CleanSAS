9d8e2fd8c33810d315cae1bcd086dcdf65eafa82
Andres Cevallos <ca.andrescevallos@gmail.com>
2025-02-19 11:14:42 -0300

    Form

--- src/Component/Main/Contact.js
+++ src/Component/Main/Contact.js
@@ -1,55 +1,34 @@
 import { faXmark } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import React, { useContext, useEffect, useState } from 'react'
+import React, { useContext, useEffect, useRef, useState } from 'react'
 import { contexto } from '../../Provider'
+import emailjs from '@emailjs/browser';
 
 export const Contact = () => {
 
     const { updateContactUs, constacUs } = useContext(contexto)
 
-    const [nameValue, setNameValue] = useState("")
-    const [emailValue, setEmailValue] = useState("")
-    const [subjectValue, setSubjectValue] = useState("")
-    const [messageValue, setMessageValue] = useState("")
 
     const background = 'https://res.cloudinary.com/dt4unqzpv/image/upload/v1739803366/chhjwnl3dmziofrlojtj.jpg'
 
-    const handleName = (e) => {
-        const name = e.target.value
-        setNameValue(name)
-    }
-
-    const handleMail = (e) => {
-        const email = e.target.value
-        setEmailValue(email)
-    }
-
-    const handleSubject = (e) => {
-        const subject = e.target.value
-        setSubjectValue(subject)
-    }
-
-    const handleMessage = (e) => {
-        const message = e.target.value
-        setMessageValue(message)
-    }
+    const form = useRef();
 
     const handleSubmit = (e) => {
         e.preventDefault();
-
-        const subject = `Consulta: ${subjectValue}`;
-        const body = `
-            Nombre: ${nameValue}
-            Email: ${emailValue}
-            Mensaje: ${messageValue}
-        `;
-
-        const mailtoUrl = `mailto:info@cleansas.com.ar?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
-
-        window.location.href = mailtoUrl;
-    };
-
-
+    
+        emailjs
+          .sendForm('service_v98gour', 'template_oi8kvx5', form.current, {
+            publicKey: 'af1nGeO0u_gGNrDms',
+          })
+          .then(
+            () => {
+              console.log('SUCCESS!');
+            },
+            (error) => {
+              console.log('FAILED...', error.text);
+            },
+          );
+      };
 
     return (
         <div className='contact'>
@@ -59,7 +38,7 @@ export const Contact = () => {
                 <article>
                     <article>
                         <h3>ENVIANOS UN CORREO</h3>
-                        <div className='mailIcon'><div><i class="bi bi-envelope"></i></div></div>
+                        <div className='mailIcon'><div><i className="bi bi-envelope"></i></div></div>
                         <strong>Te responderemos lo mas rapido posible</strong>
                         <div className='contatsIcons'>
                             <div> <i className="bi bi-whatsapp"></i></div>
@@ -70,15 +49,15 @@ export const Contact = () => {
                 </article>
 
                 <div className='formContact'>
-                    <form onSubmit={handleSubmit}>
+                    <form ref={form} onSubmit={handleSubmit}>
                         <label>Tu nombre</label>
-                        <input onChange={handleName} type="text" name="name" />
+                        <input name="user_name"  type="text"  />
                         <label>Tu correo electrónico</label>
-                        <input onChange={handleMail} type="email" name="email" />
+                        <input name="user_email"  type="email"  />
                         <label>Asunto</label>
-                        <input onChange={handleSubject} type="text" name="subject" />
+                        <input  type="text" name="subject" />
                         <label>Tu mensaje (opcional)</label>
-                        <textarea onChange={handleMessage} name="message" rows="15"></textarea>
+                        <textarea name="message"  rows="15"></textarea>
                         <div>
                             <button type='submit'>Enviar</button>
                         </div>
